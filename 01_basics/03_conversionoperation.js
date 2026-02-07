3a316f2e9ce69627f0a9e40631c4774645d04465
Ankit Roy <67535081+J0rd0n9386@users.noreply.github.com>
2026-02-07 10:35:58 +0000

    commit

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,47 @@
+//conversion of data//
+let somenumber = 33
+
+let StringNumber = String(somenumber) // yha pe hmne number ko string me convert kr diya
+//console.log(StringNumber);
+//console.log(typeof(StringNumber));
+
+///////*****************operation***********************/////////////////////////////////////////////////
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**2);
+// console.log(2**3);
+
+// let str1 = "hello" 
+// let str2 =  "Ankit"
+// let str3 = str1  +  str2
+// console.log(str3);
+// console.log("1"+2+2); //122
+// console.log(1+2+"2"); //32
+
+let gameCounter = 100
+//++gameCounter; //prefix operater
+gameCounter++; //postfix operater
+
+// example of prefix operator
+
+let a = 3;
+ const b = ++a;
+console.log(b);
+
+// link = https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+//console.log(1>2);
+//console.log(2>1);
+
+console.log("2">1);
+console.log(null> "2");
+
+
+
+
+//console.table([
+   // 2>1, 
+  //  3>2,
+     //4<3,
+    //7>2])
+    // whenever compare make sure datatype same ho
+   // console.log(null>2);
+    // comparison convert null to 0 
+
+   // console.log(undefined<0);
+
+    // === ye strict check krta hai datatype before comparison
+   // console.log("2"===2); //isme false dega cause differnt in data types
+    //console.log("2" ==2); //isme true dega
+    
+    
