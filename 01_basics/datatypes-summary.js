98efcc2cf7e044a6bb442ace7d5cee26accf446d
Ankit Roy <67535081+J0rd0n9386@users.noreply.github.com>
2026-02-07 11:19:41 +0000

    commit

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,9 @@
+//primitive 
+// 7 types: strings, Numbers, Boolean, Null, undefined, symbol,Bignit
+
+// reference type (Non Primitive)
+
+//Array, objects, Functions
+
+let userEmail;  // ye undefine ho gya
+const id = Symbol('123')
